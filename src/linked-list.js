cc5f92d26575f2fbf8d7f4aac0d4e0bfa22169f8
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-08 15:29:18 +0300

    Feat: Add LinkedList and Node classes

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,3 +1,24 @@
-export const testFun = () => {
-  return 'It works.';
-};
+class Node {
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+class LinkedList {
+  #head;
+  #tail;
+  #length = 0;
+
+  head() {
+    return this.#head ? this.#head.value : undefined;
+  }
+
+  tail() {
+    return this.#tail ? this.#tail.value : undefined;
+  }
+
+  size() {
+    return this.#length;
+  }
+}
