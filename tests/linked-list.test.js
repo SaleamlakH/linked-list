b25b084617c511ca895ecc997449c5a6977bc343
SaleamlakH <saleamlak.7h@gmail.com>
2026-02-09 17:43:52 +0300

    Test: Reorder and group tests

--- tests/linked-list.test.js
+++ tests/linked-list.test.js
@@ -7,218 +7,200 @@ describe('Linked list', () => {
     list = new LinkedList();
   });
 
-  test('Return undefined if the list is empty', () => {
-    expect(list.head()).toBe(undefined);
+  describe('Head', () => {
+    test('Return undefined for an empty list', () => {
+      expect(list.head()).toBe(undefined);
+    });
   });
 
-  test('Add a new head node', () => {
-    list.append('dog');
+  describe('Append', () => {
+    test('Add the first node', () => {
+      list.append('dog');
 
-    expect(list.head()).toBe('dog');
-    expect(list.size()).toBe(1);
+      expect(list.head()).toBe('dog');
+      expect(list.size()).toBe(1);
+    });
+
+    test('Add additional node at the end', () => {
+      list.append('dog');
+      list.append('cat');
+
+      expect(list.head()).toBe('dog');
+      expect(list.tail()).toBe('cat');
+      expect(list.size()).toBe(2);
+    });
   });
 
-  test('Append a new node at the end', () => {
-    list.append('dog');
-    list.append('cat');
+  describe('Prepend', () => {
+    test('Add a node at the start of the list', () => {
+      list.append('dog');
+      list.prepend('cat');
 
-    expect(list.head()).toBe('dog');
-    expect(list.tail()).toBe('cat');
-    expect(list.size()).toBe(2);
+      expect(list.head()).toBe('cat');
+      expect(list.at(1)).toBe('dog');
+    });
+
+    test('Update tail when prepending to an empty list', () => {
+      list.prepend('cat');
+
+      expect(list.tail()).toBe('cat');
+    });
   });
 
-  test('Return a value at index 1', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
+  describe('At', () => {
+    beforeEach(() => {
+      list.append('dog');
+      list.append('cat');
+      list.append('parrot');
+      list.append('hamster');
+      list.append('snake');
+      list.append('turtle');
+    });
 
-    expect(list.at(1)).toBe('cat');
+    test('Return a value at a given index', () => {
+      expect(list.at(0)).toBe('dog');
+      expect(list.at(1)).toBe('cat');
+      expect(list.at(4)).toBe('snake');
+    });
+
+    test('Return undefined for out of bound index', () => {
+      expect(list.at(-1)).toBe(undefined);
+      expect(list.at(6)).toBe(undefined);
+    });
   });
 
-  test('Return a value at index 4', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-    list.append('hamster');
-    list.append('snake');
-    list.append('turtle');
+  describe('Shift', () => {
+    test('Removes the head node and return its value', () => {
+      list.append('dog');
+      list.append('cat');
 
-    expect(list.at(4)).toBe('snake');
+      expect(list.shift()).toBe('dog');
+      expect(list.head()).toBe('cat');
+    });
+
+    test('Updates tail when removing the only node', () => {
+      list.append('dog');
+      list.shift();
+
+      expect(list.tail()).toBe(undefined);
+    });
+
+    test('Returns undefined when called on empty list', () => {
+      expect(list.shift()).toBe(undefined);
+    });
   });
 
-  test('Return a value at index 0', () => {
-    list.append('dog');
-    expect(list.at(0)).toBe('dog');
+  describe('Contains', () => {
+    beforeEach(() => {
+      list.append('dog');
+      list.append('cat');
+      list.append('parrot');
+      list.prepend('hamster');
+    });
+
+    test('Return true when value exists', () => {
+      expect(list.contains('cat')).toBe(true);
+      expect(list.contains('hamster')).toBe(true);
+    });
+
+    test('Return false when value does not exist', () => {
+      expect(list.contains('snake')).toBe(false);
+    });
   });
 
-  test('Return undefined if there is no node at index', () => {
-    expect(list.at(0)).toBe(undefined);
-    expect(list.at(3)).toBe(undefined);
+  describe('Find index', () => {
+    beforeEach(() => {
+      list.append('dog');
+      list.append('cat');
+      list.append('parrot');
+    });
+
+    test('Return the index of an existing value', () => {
+      expect(list.findIndex('cat')).toBe(1);
+    });
+
+    test('Return -1 when the value does not exist', () => {
+      expect(list.findIndex('hamster')).toBe(-1);
+    });
   });
 
-  test('Return undefined for negative index', () => {
-    expect(list.at(-1)).toBe(undefined);
+  describe('To string', () => {
+    test('Returns a string representation of the list', () => {
+      list.append('dog');
+      list.append('cat');
+
+      expect(list.toString()).toBe('( dog ) -> ( cat ) -> null');
+    });
   });
 
-  test('Prepend to empty list update tail', () => {
-    list.prepend('cat');
+  describe('Insert at', () => {
+    beforeEach(() => {
+      list.append('dog');
+      list.append('cat');
+    });
 
-    expect(list.tail()).toBe('cat');
+    test('Insert a node at a given index', () => {
+      list.insertAt(1, 'parrot');
+      expect(list.toString()).toBe('( dog ) -> ( parrot ) -> ( cat ) -> null');
+    });
+
+    test('Insert multiple nodes at a given index', () => {
+      list.insertAt(1, 'parrot', 'hamster');
+      expect(list.toString()).toBe(
+        '( dog ) -> ( parrot ) -> ( hamster ) -> ( cat ) -> null',
+      );
+    });
+    test('Insert a node at the start of the list', () => {
+      list.insertAt(0, 'hamster');
+      expect(list.toString()).toBe('( hamster ) -> ( dog ) -> ( cat ) -> null');
+    });
+
+    test('Insert multiple nodes at the start of the list', () => {
+      list.insertAt(0, 'hamster', 'parrot');
+      expect(list.toString()).toBe(
+        '( hamster ) -> ( parrot ) -> ( dog ) -> ( cat ) -> null',
+      );
+    });
+
+    test('Throw RangeError when index is out of bounds', () => {
+      // the function call must be wrap in a function
+      expect(() => list.insertAt(2, 'parrot')).toThrow(RangeError);
+    });
   });
 
-  test('Add a new node at the start', () => {
-    list.append('dog');
-    list.prepend('cat');
+  describe('Remove at', () => {
+    beforeEach(() => {
+      list.append('dog');
+      list.append('cat');
+      list.append('parrot');
+    });
 
-    expect(list.head()).toBe('cat');
-    expect(list.at(1)).toBe('dog');
-  });
+    test('Remove a node at a given index and return its value', () => {
+      expect(list.removeAt(1)).toBe('cat');
+    });
 
-  test('Remove the head node and return its value', () => {
-    list.append('dog');
-    list.append('cat');
+    test('Remove the last node and update the tail', () => {
+      expect(list.removeAt(2)).toBe('parrot');
+      expect(list.tail()).toBe('cat');
+    });
 
-    expect(list.shift()).toBe('dog');
-    expect(list.head()).toBe('cat');
-  });
+    test('Remove a node at index 0', () => {
+      expect(list.removeAt(0)).toBe('dog');
+    });
 
-  test('Remove update tail for single element list', () => {
-    list.append('dog');
-    list.shift();
+    test('Remove the only node in a single-node list', () => {
+      list = new LinkedList();
+      list.append('dog');
 
-    expect(list.tail()).toBe(undefined);
-  });
+      expect(list.removeAt(0)).toBe('dog');
+      expect(list.head()).toBe(undefined);
+      expect(list.tail()).toBe(undefined);
+    });
 
-  test('Remove from empty list return undefined', () => {
-    expect(list.shift()).toBe(undefined);
-  });
-
-  test('Return true if the value is in the list', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-    list.prepend('hamster');
-
-    expect(list.contains('cat')).toBe(true);
-    expect(list.contains('hamster')).toBe(true);
-  });
-
-  test('Return false if the value is not in the list', () => {
-    list.append('cat');
-    list.append('parrot');
-
-    expect(list.contains('dog')).toBe(false);
-  });
-
-  test('Return the index of a node containing a value', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-
-    expect(list.findIndex('cat')).toBe(1);
-  });
-
-  test('Return -1 if there is no node containing a value', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-
-    expect(list.findIndex('hamster')).toBe(-1);
-  });
-
-  test('Returns the list as a string', () => {
-    list.append('dog');
-    list.append('cat');
-
-    expect(list.toString()).toBe('( dog ) -> ( cat ) -> null');
-  });
-
-  test('Insert new node at a given index', () => {
-    list.append('dog');
-    list.append('cat');
-
-    list.insertAt(1, 'parrot');
-    expect(list.toString()).toBe('( dog ) -> ( parrot ) -> ( cat ) -> null');
-  });
-
-  test('Insert multiple nodes at a given index', () => {
-    list.append('dog');
-    list.append('cat');
-
-    list.insertAt(1, 'parrot', 'hamster');
-    expect(list.toString()).toBe(
-      '( dog ) -> ( parrot ) -> ( hamster ) -> ( cat ) -> null',
-    );
-  });
-
-  test('Insert nodes at index 0', () => {
-    list.append('dog');
-
-    list.insertAt(0, 'cat');
-    expect(list.toString()).toBe('( cat ) -> ( dog ) -> null');
-  });
-
-  test('Insert multiple nodes at index 0', () => {
-    list.append('dog');
-    list.append('hamster');
-
-    list.insertAt(0, 'cat', 'parrot');
-    expect(list.toString()).toBe(
-      '( cat ) -> ( parrot ) -> ( dog ) -> ( hamster ) -> null',
-    );
-  });
-
-  test('Throw RangeError if insert index is out of bounds', () => {
-    list.append('dog');
-    list.append('cat');
-
-    // the function call must be wrap in a function
-    expect(() => list.insertAt(2, 'parrot')).toThrow(RangeError);
-  });
-
-  test('Remove a node at a given index and return its value', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-
-    expect(list.removeAt(1)).toBe('cat');
-  });
-
-  test('Remove a node at the end index update the tail', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-
-    expect(list.removeAt(2)).toBe('parrot');
-    expect(list.tail()).toBe('cat');
-  });
-
-  test('Remove a node at index 0', () => {
-    list.append('dog');
-    list.append('cat');
-    list.append('parrot');
-
-    expect(list.removeAt(0)).toBe('dog');
-  });
-
-  test('Remove at index 0 from a single node list', () => {
-    list.append('dog');
-
-    expect(list.removeAt(0)).toBe('dog');
-  });
-
-  test('Remove at index 0 update tail from a single node list', () => {
-    list.append('dog');
-    list.removeAt(0);
-
-    expect(list.tail()).toBe(undefined);
-  });
-
-  test('Throw RangeError if the index is out of bound', () => {
-    list.append('dog');
-    list.append('cat');
-
-    // the call must be wrapped in a function
-    expect(() => list.removeAt(2)).toThrow(RangeError);
+    test('Throw RangeError when index is out of bound', () => {
+      // the call must be wrapped in a function
+      expect(() => list.removeAt(3)).toThrow(RangeError);
+      expect(() => list.removeAt(-1)).toThrow(RangeError);
+    });
   });
 });
